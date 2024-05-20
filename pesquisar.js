964f3c6ccde7228db547db04e6d6c7cf734323f0
Thiago Antonio <115923226+thiago-antonio@users.noreply.github.com>
2024-05-19 23:22:44 -0300

    resolvendo bugs

--- pesquisar.js
+++ pesquisar.js
@@ -2,10 +2,12 @@ const btn_pesq = document.querySelector("#btn_pesq")
 const f_txtpesq = document.querySelector("#f_txtpesq")
 
 btn_pesq.addEventListener("click",(evt)=>{
-    if(f_txtpesq.value==""){
+    const valorpesq = f_txtpesq.value
+    if(valorpesq==""){
         alert("digite a pesquisa")
         f_txtpesq.focus()
         return
     }
-    console.log(document.getElementsByName("f_pesq").value)
+    const f_pesq = document.querySelector("input[name=f_pesq]:checked").value
+    console.log(f_pesq)
 })
