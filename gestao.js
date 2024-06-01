aeb7334d47f57ee311a8e5d7f0f9541e4e26749c
Thiago Antonio <115923226+thiago-antonio@users.noreply.github.com>
2024-05-31 23:35:44 -0300

    finalizando o back end, e criando uma api em php

--- gestao.js
+++ gestao.js
@@ -8,6 +8,11 @@ const f_celular = document.querySelector("#f_celular")
 const f_email = document.querySelector("#f_email")
 const f_dtnasc = document.querySelector("#f_dtnasc")
 
+fetch("gestao.php")
+.then(res => res.json())
+.then(res=>{
+    console.log(res)
+})
 
 btn_gravar.addEventListener("click",(evt)=>{
     Fundopopup.classList.add("ocultar")
